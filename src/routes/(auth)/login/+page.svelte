93ecc6cd7cd723ab42250109af5c7d6abc47c12b
Junyoung <compilevoid@gmail.com>
2025-11-24 08:51:38 +0900

    Create +page.svelte

---
+++ src/routes/(auth)/login/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  let username = '';
+  let password = '';
+</script>
+
+<form method="POST">
+  <input name="username" bind:value={username} placeholder="아이디" />
+  <input name="password" type="password" bind:value={password} placeholder="비밀번호" />
+  <button type="submit">로그인</button>
+</form>
+
+{#if $page.form?.error}
+  <p style="color:red;">{$page.form.error}</p>
+{/if}
